5174ceb8904ffd289375ae3a0aecc0b5db4bf0e3
DAYANANDA CHALLA <10610231+dayananDchallA@users.noreply.github.com>
2023-12-02 10:28:53 +0530

    Add files via upload

---
+++ hello_taipy.py
@@ -0,0 +1,9 @@
+from taipy import Gui
+
+page = """
+# Hello World 🌍 with *Taipy*This is my first Taipy test app.
+And it is running fine!
+"""
+
+Gui(page).run(use_reloader=True)  # use_reloader=True if you are in development
+
